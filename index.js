faa308b850253154f25742c214584370b8bda3b7
Tomas Viglini <tomas.gonzalo@viglini.com>
2017-04-15 23:40:40 -0300

    Add listen

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World v2");
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World v2\n');
+});
+
+server.listen(port, hostname);
