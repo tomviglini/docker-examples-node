66171c9385a0678a82008781767efd1249ea4b0e
Tomas Viglini <tomas.gonzalo@viglini.com>
2017-04-18 02:47:37 -0300

    production

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World Staging\n');
+  res.end('Hello World Production\n');
 });
 
 server.listen(port, hostname);
