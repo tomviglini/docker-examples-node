d3f0a5da730be66e4b96e7019a3ee624b1947424
Tomas Viglini <tomas.gonzalo@viglini.com>
2017-04-23 13:31:24 -0300

    asd

--- pub-sub/producer/index.js
+++ pub-sub/producer/index.js
@@ -1,29 +1,28 @@
 'use strict';
 
-const PubSub = require(`@google-cloud/pubsub`);
+//const PubSub = require(`@google-cloud/pubsub`);
 
 
-/*var pubsub = require('@google-cloud/pubsub')({
+var pubsub = require('@google-cloud/pubsub')({
   projectId: 'viglini-1',
 //  keyFilename: '/path/to/keyfile.json'
-});*/
+});
 
 const express = require('express');
 const app = express();
 
 function publishMessage (topicName, data) {
-  const pubsub = PubSub({
-    projectId: 'viglini-1'
-  });
+  //const pubsub = PubSub();
   const topic = pubsub.topic(topicName);
-  return topic.publish(data)
-    .then((results) => {
-      const messageIds = results[0];
-      console.log(`Message ${messageIds[0]} published.`);
-      return messageIds;
-    }).catch((a) => {
-        console.log(a);
-    });
+  topic.publish(data);
+//  return topic.publish(data)
+//    .then((results) => {
+//      const messageIds = results[0];
+//      console.log(`Message ${messageIds[0]} published.`);
+//      return messageIds;
+//    }).catch((a) => {
+//        console.log(a);
+//    });
 }
 
 app.get('/pub-sub/producer', function (req, res) {
@@ -31,11 +30,11 @@ app.get('/pub-sub/producer', function (req, res) {
     date: new Date().toString(),
     number: Math.random(1, 100000)
   });
-  res.send('Hello from producer')
+  res.send('Hello from producer');
 });
 
 app.get('*', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(80);
